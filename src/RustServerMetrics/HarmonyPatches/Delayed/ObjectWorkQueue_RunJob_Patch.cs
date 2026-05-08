268c0062b203fc184281c5443e406c69da7578e7
JJ <jexs@moose.gg>
2026-05-08 17:52:10 -0500

    update ObjectWorkQueue_RunJob_Patch

--- src/RustServerMetrics/HarmonyPatches/Delayed/ObjectWorkQueue_RunJob_Patch.cs
+++ src/RustServerMetrics/HarmonyPatches/Delayed/ObjectWorkQueue_RunJob_Patch.cs
@@ -2,13 +2,13 @@
 using RustServerMetrics.HarmonyPatches.Utility;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
 using UnityEngine;
 using Stopwatch = System.Diagnostics.Stopwatch;
 
 // ReSharper disable InconsistentNaming
+// ReSharper disable PossibleMultipleEnumeration
 
 namespace RustServerMetrics.HarmonyPatches.Delayed;
 
@@ -31,20 +31,23 @@ internal static class ObjectWorkQueue_RunJob_Patch
     }
 
     [HarmonyTargetMethods]
-    public static IEnumerable<MethodBase> TargetMethods(Harmony harmonyInstance)
+    public static IEnumerable<MethodBase> TargetMethods()
     {
         var assemblyCSharp = typeof(BaseNetworkable).Assembly;
         var typesToScan = new Stack<Type>(assemblyCSharp.GetTypes());
-        HashSet<string> yielded = [];
-
+        var yielded = new HashSet<string>();
+        
         while (typesToScan.TryPop(out var type))
         {
-            var subTypes = type.GetNestedTypes();
-            foreach (var t in subTypes)
+            foreach (var t in type.GetNestedTypes())
+            {
                 typesToScan.Push(t);
+            }
 
             if (type.BaseType == null || !type.BaseType.Name.Contains("ObjectWorkQueue"))
+            {
                 continue;
+            }
 
             if (yielded.Add(type.FullName))
             {
@@ -54,32 +57,57 @@ internal static class ObjectWorkQueue_RunJob_Patch
     }
 
     [HarmonyTranspiler]
-    public static IEnumerable<CodeInstruction> Transpile(IEnumerable<CodeInstruction> originalInstructions,
-                                                         MethodBase methodBase,
-                                                         ILGenerator ilGenerator)
+    public static IEnumerable<CodeInstruction> Transpiler(
+        IEnumerable<CodeInstruction> instructions,
+        MethodBase methodBase,
+        ILGenerator ilGenerator)
     {
-        var ret = originalInstructions.ToList();
-        var local = ilGenerator.DeclareLocal(typeof(long));
-
-        ret.InsertRange(0, [
-            new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Stopwatch), nameof(Stopwatch.GetTimestamp))),
-            new CodeInstruction(OpCodes.Stloc, local)
-        ]);
-
-        return Helpers.Postfix(ret,
-                               CustomPostfix,
-                               new CodeInstruction(OpCodes.Ldstr, $"{methodBase.DeclaringType?.Name}.{methodBase.Name}"),
-                               new CodeInstruction(OpCodes.Ldloc, local));
+        try
+        {
+            var invokedMethod = $"{methodBase.DeclaringType?.Name}.{methodBase.Name}";
+            
+            var matcher = new CodeMatcher(instructions, ilGenerator)
+                .Start()
+                .DeclareLocal(typeof(long), out var timestampLocal)
+                .InsertAndAdvance(
+                    new CodeInstruction(
+                        OpCodes.Call, 
+                        AccessTools.Method(
+                            typeof(Stopwatch), 
+                            nameof(Stopwatch.GetTimestamp))),
+                    new CodeInstruction(OpCodes.Stloc, timestampLocal))
+                .MatchStartForward(new CodeMatch(OpCodes.Ret))
+                .Repeat(repeatingMatcher =>
+                {
+                    CodeInstruction[] toInsert =
+                    [
+                        new(OpCodes.Ldstr, invokedMethod),
+                        new(OpCodes.Ldloc, timestampLocal),
+                        new(OpCodes.Call, AccessTools.Method(typeof(ObjectWorkQueue_RunJob_Patch), nameof(RecordInvokeTime)))
+                    ];
+                    
+                    repeatingMatcher.Instruction.MoveLabelsTo(toInsert[0]);
+                    repeatingMatcher.InsertAndAdvance(toInsert);
+                    repeatingMatcher.Advance(1);
+                });
+            
+            return matcher.Instructions();
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"[ServerMetrics] {nameof(ObjectWorkQueue_RunJob_Patch)}: " + e.Message);
+            return instructions;
+        }
     }
 
-    private static void CustomPostfix(string methodName, long __state)
+    private static void RecordInvokeTime(string methodName, long startTimestamp)
     {
         if (!MetricsLogger.IsReady)
         {
             return;
         }
 
-        var ms = (Stopwatch.GetTimestamp() - __state) * TicksToMs;
+        var ms = (Stopwatch.GetTimestamp() - startTimestamp) * TicksToMs;
         MetricsLogger.Instance.WorkQueueTimes.LogTime(methodName, ms);
     }
 }
